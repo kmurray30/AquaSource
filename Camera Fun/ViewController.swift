188ed5a0e1e0baa14dacdad59c1f93a2a7187fff
kmurray30 <kyle.murray100@gmail.com>
2016-10-22 01:38:19 -0400

    "Commit camera app"

--- Camera Fun/ViewController.swift
+++ Camera Fun/ViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var pickedImage: UIImageView!
+    @IBOutlet weak var isCameraAvailableLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,36 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func cameraButtonAction(_ sender: AnyObject) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.camera
+            ;
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        } else {
+            isCameraAvailableLabel.text = "Camera not available"
+            isCameraAvailableLabel.textColor = UIColor.init(red: 0.961, green: 0.957, blue: 0945, alpha: 1)
+        }
+    }
+    
+    @IBAction func photoLibraryAction(_ sender: AnyObject) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.photoLibrary) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+            ;
+            imagePicker.allowsEditing = true
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+        pickedImage.image = image
+        self.dismiss(animated: true, completion: nil);
+    }
+    
 
 }
 
